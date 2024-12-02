be40a206b3c7daf39d1ba2f45d0b4f974b7d0842
Sufiyanmanihar747 <sufiyanmanihar52.com>
2024-12-02 21:02:42 +0530

    first commit

---
+++ project/backend/app/config.py
@@ -0,0 +1,5 @@
+# import os
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')  # Security for sessions
+#     UPLOAD_FOLDER = 'uploads/'
---
+++ project/index.py
@@ -0,0 +1,13 @@
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world! this is new'
+
+# @app.route('/about')
+# def hello_world1():
+#     return 'this is new'
+
+
+# if __name__ == '__main__':
+#     app.run()
